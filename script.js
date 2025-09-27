fc86144901ec6a5b902a57ea8fea7b4a26640776
Valentin <mr.dubik.v@yandex.ru>
2025-09-27 21:55:20 +0300

    rename id link

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.getElementById('link').addEventListener('click', function(event) {
-        // Предотвращаем стандартное действие ссылки (переход и изменение URL)
-        event.preventDefault();
+document
+  .getElementById("questionsLink")
+  .addEventListener("click", function (event) {
+    // Предотвращаем стандартное действие ссылки (переход и изменение URL)
+    event.preventDefault();
 
-        // Находим целевой элемент
-        const targetElement = document.getElementById('questions');
+    // Находим целевой элемент
+    const targetElement = document.getElementById("questions");
 
-        // Прокручиваем к целевому элементу
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth' // Для плавной прокрутки
-            });
-        }
-    });
+    // Прокручиваем к целевому элементу
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth", // Для плавной прокрутки
+      });
+    }
+  });
